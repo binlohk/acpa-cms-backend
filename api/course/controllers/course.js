62678ca32186bbdd5e373e921e88b7a09fd8029e
Your Name <you@example.com>
2021-05-20 12:10:50 +0800

    added check if lesson is purchased and finished

--- api/course/controllers/course.js
+++ api/course/controllers/course.js
@@ -6,30 +6,43 @@ const { sanitizeEntity } = require('strapi-utils');
  * to customize this controller
  */
 
+const customizeEntityValue = (entity) => {
+    const entityWithoutPrivateField = sanitizeEntity(entity, { model: strapi.models.course });
+    return {
+        lessonsDetail: entity.lessons.map(lesson=>{return { id: lesson.id, title: lesson.title, text: lesson.lessonDescription, finished: false }}), 
+        courseMaterials: entity.course_materials.map(material=>{ return { id: material.id, title: material.title }}),
+        ...entityWithoutPrivateField
+    };
+}
+
+const checkIfUserFinishedLesson = async ( entity, userId ) => {
+    for(let i=0; i<entity.lessonsDetail.length; i++){
+        let lessonId = entity.lessonsDetail[i].id;
+        const lessonProgressRecord = await strapi.services['user-progress'].findOne({lesson: lessonId, 'users_permissions_user': userId});
+        if(lessonProgressRecord){
+            entity.lessonsDetail[i].finished = true;
+        }
+    }
+}
+
 module.exports = {
     async find(ctx) {
-        let entities;
-        if (ctx.query._q) {
-          entities = await strapi.services.course.search(ctx.query);
-        } else {
-          entities = await strapi.services.course.find(ctx.query);
-        }
-
-        return entities.map(entity => { 
-            return {
-                lessonsDetail: entity.lessons.map(lesson=>{return { id: lesson.id, title: lesson.title, text: lesson.lessonDescription}}), 
-                courseMaterials: entity.course_materials.map(material=>{ return { id: material.id, title: material.title}}),
-                ...sanitizeEntity(entity, { model: strapi.models.course })};
-        });
+        let entities = await strapi.services.course.find(ctx.query);
+        const customizedEntities = entities.map(entity => customizeEntityValue(entity));
+        if(ctx.state.user){
+            for(let customizedEntity of customizedEntities){
+                await checkIfUserFinishedLesson(customizedEntity, ctx.state.user.id);
+            }
+        } 
+        return customizedEntities;
     },
     async findOne(ctx) {
         const { id } = ctx.params;
         const entity = await strapi.services.course.findOne({ id });
-        const entityWithoutPrivateField = sanitizeEntity(entity, { model: strapi.models.course });
-        return {
-            lessonsDetail: entity.lessons.map(lesson=>{return { id: lesson.id, title: lesson.title, text: lesson.lessonDescription}}), 
-            courseMaterials: entity.course_materials.map(material=>{ return { id: material.id, title: material.title }}),
-            ...entityWithoutPrivateField
-        };
+        const customizedEntity = customizeEntityValue(entity)
+        if(ctx.state.user){
+            await checkIfUserFinishedLesson(customizedEntity, ctx.state.user.id);
+        } 
+        return customizedEntity;
     },
 };
