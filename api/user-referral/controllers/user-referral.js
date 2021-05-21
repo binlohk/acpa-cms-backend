c0be9f89daa4338fb378ba5b57267f2cf8b17ff5
Your Name <you@example.com>
2021-05-21 15:09:57 +0800

    implemented user referral

--- api/user-referral/controllers/user-referral.js
+++ api/user-referral/controllers/user-referral.js
@@ -1,8 +1,15 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async create(ctx) {
+        console.log(ctx.request.body)
+        let entity = await strapi.services['user-referral'].create(ctx.request.body);
+        return sanitizeEntity(entity, { model: strapi.models['user-referral'] });
+      },
+};
