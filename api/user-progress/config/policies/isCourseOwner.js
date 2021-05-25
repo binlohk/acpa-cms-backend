0cbf85657ed0e3cf47b3882e249e949ffeb8f39a
Your Name <you@example.com>
2021-05-25 14:49:30 +0800

    added user lesson progress APIs

---
+++ api/user-progress/config/policies/isCourseOwner.js
@@ -0,0 +1,18 @@
+module.exports = async (ctx, next) => {
+    let targetLesson;
+    try{
+        const { lesson } = ctx.request.body;
+        targetLesson = await strapi.services.lesson.findOne({ id: lesson });
+        const targetCourseId = targetLesson.course.id;
+        const user = await strapi.query('user', 'users-permissions').findOne({id: ctx.state.user.id});
+        const userCourses = user.courses;
+        const ifUserOwnTargetCourse = userCourses.filter(course=>{return (course.id == targetCourseId)});
+        if (ifUserOwnTargetCourse.length==1){
+            return await next();
+        } else {
+            ctx.unauthorized(`You haven't purchased the course yet.`);
+        }
+    } catch (e) {
+        return ctx.badRequest('Lesson does not exist.');
+    }
+};
