b117c42729543e8a8ce39a8933316af37991d950
Winnie-Wong-Binlo <winnie@binlo.io>
2021-06-29 18:15:13 +0800

    modify config/server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '174.138.20.136'),
+  host: env('HOST', 'app.acpa.training'),
   port: env.int('PORT', 1337),
-  url: 'http://app.acpa.training/api',
+  url: 'http://app.acpa.training',
   admin: {
     auth: {
+      url: 'http://app.acpa.training/dashboard',
       secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
     },
   },
