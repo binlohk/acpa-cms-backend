b3850d4096a94bced4a8989404459556d059bd83
Winnie-Wong-Binlo <winnie@binlo.io>
2021-06-29 17:45:02 +0800

    add config in server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '174.138.20.136'),
   port: env.int('PORT', 1337),
+  url: 'http://app.acpa.training/api',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
