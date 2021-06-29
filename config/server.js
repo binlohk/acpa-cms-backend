867561465c30efd321663c458496208d54995b96
Winnie-Wong-Binlo <winnie@binlo.io>
2021-06-29 10:39:04 +0800

    edit config/server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '0.0.0.0', '174.138.20.136'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
@@ -7,3 +7,4 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
