2f890ee59baaeb54b11d02ed16e40583868df553
Winnie-Wong-Binlo <winnie@binlo.io>
2021-07-02 18:06:47 +0800

    config/server.js

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'http://app.acpa.training'),
   port: env.int('PORT', 1337),
   url: `http://app.acpa.training/api`,
   admin: {
