9bef070a928d9d8e7d79fc58718fb008153fa1da
Winnie Wong <winnie@shergon.com>
2021-07-09 15:38:59 +0800

    push

---
+++ config/env/production/server.js
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: `http://app.acpa.training`,
+  admin: {
+    url: `http://app.acpa.training/admin`,
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
+    },
+  },
+});
