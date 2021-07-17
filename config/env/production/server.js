bb5edfa621dab3b3737e5af16a72c04f7b194067
michaellee8 <ckmichael8@gmail.com>
2021-07-17 11:34:26 +0800

    fix: admin panel on local development

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'http://app.acpa.training'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: `http://app.acpa.training/api`,
   admin: {
--- config/server.js
+++ config/server.js
@@ -1,11 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'localhost'),
+  host: env('HOST', '0.0.0.0', '174.138.20.136'),
   port: env.int('PORT', 1337),
-  url: `http://localhost:1337/api`,
   admin: {
-    url: `http://localhost:1337/admin`,
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
     },
   },
 });
+
