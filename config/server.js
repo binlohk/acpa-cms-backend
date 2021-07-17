6f2b200ab23c2b3a42164619caf68a1f333ec37b
michaellee8 <ckmichael8@gmail.com>
2021-07-17 11:34:26 +0800

    fix: admin panel on local development

---
+++ config/server.js
@@ -0,0 +1,10 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0', '174.138.20.136'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
+    },
+  },
+});
+
