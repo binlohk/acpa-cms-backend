aff553b3f3715082763fd4f987828ca6d8e7d337
michaellee8 <ckmichael8@gmail.com>
2021-07-20 21:00:01 +0800

    feat: add STRAPI_URL env var for configuring url on production

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0', '174.138.20.136'),
   port: env.int('PORT', 1337),
+  url: env('STRAPI_URL', ''),
   admin: {
+    url: env('STRAPI_ADMIN_URL', '/admin'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
     },
