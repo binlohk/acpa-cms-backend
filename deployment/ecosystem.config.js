e4ddf019b044deed604df7a58766d6395ba855c6
michaellee8 <ckmichael8@gmail.com>
2021-08-02 20:32:57 +0800

    fix: https config

--- deployment/ecosystem.config.js
+++ deployment/ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
       env: {
         "NODE_ENV": "production",
         "DATABASE_FILENAME": "/home/ubuntu/deploy-production/data/acpa-backend.db",
-        "STRAPI_URL": "http://app.acpa.training/api",
-        "STRAPI_ADMIN_URL": "http://app.acpa.training/api/admin",
+        "STRAPI_URL": "https://app.acpa.training/api",
+        "STRAPI_ADMIN_URL": "https://app.acpa.training/admin",
       },
     }
   ]
