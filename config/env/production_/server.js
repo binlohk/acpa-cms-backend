5d52544d91ae6c6de71f21c40871af0e93db6a9d
Winnie-Wong-Binlo <82791204+Winnie-Wong-Binlo@users.noreply.github.com>
2021-08-03 11:59:58 +0800

    Update server.js

--- config/env/production_/server.js
+++ config/env/production_/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: env('STRAPI_URL', ''),
   admin: {
-    url: env('STRAPI_ADMIN_URL', '/admin'),
+    url: env('/admin'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
     },
