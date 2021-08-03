d57dace24c7bbb7d80b385b28f482a4ae3c61c87
Winnie-Wong-Binlo <82791204+Winnie-Wong-Binlo@users.noreply.github.com>
2021-08-03 11:27:07 +0800

    Update server.js

--- config/env/production_/server.js
+++ config/env/production_/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: `http://app.acpa.training/api`,
+  url: env('STRAPI_URL', ''),
   admin: {
-    url: `http://app.acpa.training/admin`,
+    url: env('STRAPI_ADMIN_URL', '/admin'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'ce03286d6f96852ce508abc8a0e11d48'),
     },
