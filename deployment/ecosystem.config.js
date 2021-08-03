3a95f4f5258aedd42a487025760845cb0bd6e62a
Winnie-Wong-Binlo <82791204+Winnie-Wong-Binlo@users.noreply.github.com>
2021-08-03 11:39:16 +0800

    Update ecosystem.config.js

--- deployment/ecosystem.config.js
+++ deployment/ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         "NODE_ENV": "production",
         "DATABASE_FILENAME": "/home/ubuntu/deploy-production/data/acpa-backend.db",
         "STRAPI_URL": "https://app.acpa.training/api",
-        "STRAPI_ADMIN_URL": "https://app.acpa.training/admin",
+        "STRAPI_ADMIN_URL": "https://app.acpa.training/api/admin",
         "HOST": "0.0.0.0",
       },
     }
