b8b879b0c5090171ac43d57ac5bdf7d977c1bde9
michaellee8 <ckmichael8@gmail.com>
2021-08-08 09:32:30 +0800

    feat: implement basic push notification without error checking yet

--- api/notification/models/notification.js
+++ api/notification/models/notification.js
@@ -1,4 +1,7 @@
 'use strict';
+const { Expo } = require('expo-server-sdk');
+
+const expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
@@ -8,7 +11,34 @@
 module.exports = {
   lifecycles: {
     async afterCreate(result, data){
-
+      const users = await strapi
+        .query('user', 'users-permissions')
+        .find({ _limit: -1 });
+      const messages = users
+        .filter(user => user.pushNotificationToken)
+        .map(user => user.pushNotificationToken)
+        .filter(user => Expo.isExpoPushToken)
+        .map(token => ({
+          to: token,
+          sound: 'default',
+          title: result.title,
+          body: result.description,
+        }));
+      let chunks = expo.chunkPushNotifications(messages);
+      let tickets = [];
+      for (let chunk of chunks) {
+        try {
+          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+          console.log(ticketChunk);
+          tickets.push(...ticketChunk);
+          // NOTE: If a ticket contains an error code in ticket.details.error, you
+          // must handle it appropriately. The error codes are listed in the Expo
+          // documentation:
+          // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+        } catch (error) {
+          console.error(error);
+        }
+      }
     },
   },
 };
