1f11cce76d1ef43f130cbf6bbf1adc60b54d34be
Winnie-Wong-Binlo <winnie@binlo.io>
2021-08-11 16:23:37 +0800

    remove test email

--- config/plugins.js
+++ config/plugins.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
       apiKey: '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',
     },
     settings: {
-      defaultFrom: 'mark@binlo.io',
-      defaultReplyTo: 'mark@binlo.io',
-      testAddress: 'wongw859@gmail.com',
+      defaultFrom: '',
+      defaultReplyTo: '',
+      testAddress: '',
     },
 
   },
