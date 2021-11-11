52afd73fcdf6ac5ac3bf5f1ae20455c26dc4342f
Meet Patel <meet@binlo.com.hk>
2021-11-11 17:04:56 +0530

    content-manager plugin overrides in /extensions
    content-manager plugin overrides in /extensions  to change sorting.

---
+++ extensions/content-manager/admin/src/containers/ListView/utils/buildQueryString.js
@@ -0,0 +1,27 @@
+import { stringify } from 'qs';
+import createPluginsFilter from './createPluginsFilter';
+
+/**
+ * Creates a valid query string from an object of queryParams
+ * This includes:
+ * - a _where clause
+ * - plugin options
+ */
+const buildQueryString = (queryParams = {}) => {
+  queryParams._sort = "updated_at:DESC";  // sort all data by default lastupdate date DESC
+  const _where = queryParams._where || [];
+
+  /**
+   * Extracting pluginOptions from the query since we don't want them to be part
+   * of the url
+   */
+  const { plugins: _, ...otherQueryParams } = {
+    ...queryParams,
+    _where,
+    ...createPluginsFilter(queryParams.plugins),
+  };
+
+  return `?${stringify(otherQueryParams, { encode: false })}`;
+};
+
+export default buildQueryString;
